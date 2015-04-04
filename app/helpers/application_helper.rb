fabf5327e1e8fed1edec4004b135ce7e7a143a6c
Reutskiy Sergey <reutskiy@braintent.com>
2015-04-04 14:27:29 +0300

    Create first page and navigation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def Icon64(name)
+
+    html_safe "
+        <svg class='icon_svg'><use xlink:href='##{name}'/></svg>
+    "
+  end
+
+  def html_safe(html)
+    html.respond_to?(:html_safe) ? html.html_safe : html
+  end
 end
