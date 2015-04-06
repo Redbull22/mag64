dc7dd07c30714315c591573891fd79a12c5f0a9f
Reutskiy Sergey <reutskiy@braintent.com>
2015-04-06 22:13:21 +0300

    First complete version

--- app/assets/javascripts/required/general_functions.js
+++ app/assets/javascripts/required/general_functions.js
@@ -1,18 +1,14 @@
 $(document).ready(function() {
 
-  setContentHeight();
-  $('.js--truncate').truncate({
-    token: '&hellip;',
-    multiline: true,
-    addfulltext: true
-  });
+
 
 });
 
 $( window ).resize(function() {
-  setContentHeight();
+  // setContentHeight();
+  restoreFullContent();
   $('.js--truncate').truncate({
-    token: '&hellip; <a href="#" onclick="showFullContent(this)">далее</a>',
+    token: '&hellip;',
     multiline: true,
     addfulltext: true
   });
@@ -26,10 +22,35 @@ var setContentHeight = function() {
 }
 
 function showFullContent(elem) {
-  $('.content64__center').find('img').hide();
+  $('.content64__center__image').find('img').css({'opacity': 0});
   var representContainer = document.getElementById('js--content_represent');
-  representContainer.style.display='block';
+  $(representContainer).closest('.content64__center__represent_container').fadeIn().css({'z-index': 10});
 
-  representContainer.innerHTML = $(elem.parentNode).data('fulltext');
+  var dataRubric = $(elem).closest('.content64').data('rubric');
+  if (dataRubric != '')
+    $(representContainer).siblings('.content64__center__represent_container__rubric').html(dataRubric);
 
+  representContainer.innerHTML = $(elem).closest('.row').siblings('.black_header').html() + $(elem).find('.js--truncate').data('fulltext');
+}
+
+function showBottomContent(elem) {
+  $('.content64__center__image').find('img').css({'opacity': 0});
+  var representContainer = document.getElementById('js--content_represent');
+  $('.content64__center__represent_container__rubric').html('');
+  $(representContainer).closest('.content64__center__represent_container').fadeIn().css({'z-index': 10});
+  representContainer.innerHTML = '<h2>' + $(elem).find('.content64__bottom__link__header').html() + '</h2>' + $(elem).find('.content64__bottom__link__text').html();
+}
+
+function restoreFullContent(elem) {
+  for(var i=0, size = $('.js--truncate').length; i<size; i++){
+    $($('.js--truncate')[i]).html($($('.js--truncate')[i]).data('fulltext'));
+  }
+}
+
+function truncateText() {
+  $('.js--truncate').truncate({
+    token: '&hellip;',
+    multiline: true,
+    addfulltext: true
+  });
 }
